489ebcb28b3431cb7bc3127975a74ec97e0c2d65
arnabb38 <arnabbasak896@gmail.com>
2021-03-13 21:51:59 +0600

    get individual topic using PathVariable

--- course-api/src/main/java/com/arnabb/springbootstarter/topic/TopicController.java
+++ course-api/src/main/java/com/arnabb/springbootstarter/topic/TopicController.java
@@ -1,6 +1,7 @@
 package com.arnabb.springbootstarter.topic;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +18,11 @@ public class TopicController {
     public List<Topic> GetAllTopics()
     {
         return topicService.GetAllTopics();
+    }
+
+    @RequestMapping("/topics/{id}")
+    public Topic getTopic(@PathVariable String id)
+    {
+        return topicService.getTopic(id);
     }
 }
