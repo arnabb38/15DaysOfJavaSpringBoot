4e57de957817b4e32f4fbed5a6818fd1a54da551
arnabb38 <arnabbasak896@gmail.com>
2021-03-15 00:38:09 +0600

    initial commit by adding POM dependencies

---
+++ course-api-data/src/main/java/com/arnabb/topic/TopicController.java
@@ -0,0 +1,48 @@
+package com.arnabb.topic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class TopicController {
+
+    @Autowired
+    private TopicService topicService;
+
+    // GET request
+    @RequestMapping("/topics")
+    public List<Topic> GetAllTopics()
+    {
+        return topicService.GetAllTopics();
+    }
+
+    // GET request
+    @RequestMapping("/topics/{id}")
+    public Topic getTopic(@PathVariable String id)
+    {
+        return topicService.getTopic(id);
+    }
+
+    // POST request
+    @RequestMapping(method = RequestMethod.POST, value = "/topics")
+    public void addTopic(@RequestBody Topic topic)
+    {
+        topicService.addTopic(topic);
+    }
+
+    // PUT request
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String id)
+    {
+        topicService.updateTopic(id, topic);
+    }
+
+    // DELETE request
+    @RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
+    public void deleteTopic(@PathVariable String id)
+    {
+        topicService.deleteTopic(id);
+    }
+}
---
+++ course-api-data/src/main/java/com/arnabb/topic/TopicService.java
@@ -0,0 +1,54 @@
+package com.arnabb.topic;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+
+    // Array.asList is immutable, can't be manipulated (add or delete)
+    // ArrayList<> added to give it mutability and passes Array.asList as an object
+    private List<Topic> topics = new ArrayList<>(Arrays.asList(
+            new Topic("spring", "Spring Framework", "Spring Framework Description"),
+            new Topic("java", "Core Java", "Core Java Description"),
+            new Topic("python", "Python 3", "Python 3 Description"),
+            new Topic("flutter", "Flutter", "Flutter Description")
+    ));
+
+    public List<Topic> GetAllTopics()
+    {
+        return topics;
+    }
+
+    public Topic getTopic(String id)
+    {
+        return topics.stream().filter(t -> t.getId()
+                .equals(id))
+                .findFirst()
+                .get();
+    }
+
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    public void updateTopic(String id, Topic topic) {
+        for(int i = 0; i<topics.size(); i++)
+        {
+            Topic t = topics.get(i);
+            if(t.getId().equals(id))
+            {
+                topics.set(i, topic);
+                return;
+            }
+        }
+    }
+
+    public void deleteTopic(String id) {
+        topics.removeIf(t -> t.getId().equals(id));
+    }
+}
