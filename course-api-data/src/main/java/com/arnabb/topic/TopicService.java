449617f9b1e499de6ca9dbd1d9a64555fea9786f
arnabb38 <arnabbasak896@gmail.com>
2021-03-15 01:10:12 +0600

    connected to an embedded apache derby database and implement CRUD operation

--- course-api-data/src/main/java/com/arnabb/topic/TopicService.java
+++ course-api-data/src/main/java/com/arnabb/topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.arnabb.topic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,46 +10,35 @@ import java.util.List;
 @Service
 public class TopicService {
 
-    // Array.asList is immutable, can't be manipulated (add or delete)
-    // ArrayList<> added to give it mutability and passes Array.asList as an object
-    private List<Topic> topics = new ArrayList<>(Arrays.asList(
-            new Topic("spring", "Spring Framework", "Spring Framework Description"),
-            new Topic("java", "Core Java", "Core Java Description"),
-            new Topic("python", "Python 3", "Python 3 Description"),
-            new Topic("flutter", "Flutter", "Flutter Description")
-    ));
+    @Autowired
+    private TopicRepository topicRepository;
 
     public List<Topic> GetAllTopics()
     {
+        List<Topic> topics = new ArrayList<>();
+        topicRepository.findAll()
+                .forEach(topics::add);
         return topics;
     }
 
     public Topic getTopic(String id)
     {
-        return topics.stream().filter(t -> t.getId()
-                .equals(id))
-                .findFirst()
-                .get();
+        return topicRepository.findOne(id);
     }
 
 
-    public void addTopic(Topic topic) {
-        topics.add(topic);
+    public void addTopic(Topic topic)
+    {
+        topicRepository.save(topic);
     }
 
-    public void updateTopic(String id, Topic topic) {
-        for(int i = 0; i<topics.size(); i++)
-        {
-            Topic t = topics.get(i);
-            if(t.getId().equals(id))
-            {
-                topics.set(i, topic);
-                return;
-            }
-        }
+    public void updateTopic(String id, Topic topic)
+    {
+        topicRepository.save(topic);
     }
 
-    public void deleteTopic(String id) {
-        topics.removeIf(t -> t.getId().equals(id));
+    public void deleteTopic(String id)
+    {
+        topicRepository.delete(id);
     }
 }
